6d74720e5dbb760fe2cd6543d2e536e4143df162
DevOps <daljeet@essentialdesigns.net>
2020-03-12 20:41:33 +0530

    build fixes

--- dist/server.js
+++ dist/server.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
+const App_1 = __importDefault(require("./App"));
 const http_1 = __importDefault(require("http"));
 const PORT = 3000;
-const server = http_1.default.createServer(app_1.default);
+const server = http_1.default.createServer(App_1.default);
 exports.server = server;
 server.listen(PORT, () => {
     console.log('Example app listening on port 3000!');
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 
-import app from "./app";
+import app from "./App";
 import http from 'http';
 const PORT = 3000;
 
